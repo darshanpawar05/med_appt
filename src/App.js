fb514ff2e3600bc09674f2711f2366a8eb1982ce
Darshan Pawar <darshan.pawar23@gmail.com>
2026-04-20 20:00:28 +0530

    fixed router error

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
 import SignUp from "./pages/Sign_Up";
@@ -7,42 +7,25 @@ import Login from "./pages/Login";
 import FindDoctorSearch from "./pages/FindDoctorSearch";
 
 function App() {
-  // 🔔 Notification state
   const [message, setMessage] = useState("");
 
-  // 🔔 Show notification
   const showNotification = (msg) => {
     setMessage(msg);
     setTimeout(() => setMessage(""), 3000);
   };
 
   return (
-    <Router>
-      <div>
-        {/* Navbar */}
-        <Navbar />
+    <div>
+      <Navbar />
 
-        {/* 🔔 THIS LINE IS VERY IMPORTANT FOR MARKS */}
-        {message && (
-          <p style={{ textAlign: "center", color: "green" }}>
-            {message}
-          </p>
-        )}
+      {message && <p style={{ textAlign: "center" }}>{message}</p>}
 
-        <Routes>
-          <Route path="/" element={<SignUp />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/search" element={<FindDoctorSearch />} />
-        </Routes>
-
-        {/* Test button */}
-        <div style={{ textAlign: "center", marginTop: "20px" }}>
-          <button onClick={() => showNotification("Action Successful!")}>
-            Test Notification
-          </button>
-        </div>
-      </div>
-    </Router>
+      <Routes>
+        <Route path="/" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/search" element={<FindDoctorSearch />} />
+      </Routes>
+    </div>
   );
 }
 
